35bf5073b8a75d00065808dfb1c6fb7d96aa0dbf
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-06-25 20:50:09 -0700

    5.2. Consume an IDL in Typescript

---
+++ prereq-ts/airdrop/enroll.ts
@@ -0,0 +1,18 @@
+import { Connection, Keypair, PublicKey } from "@solana/web3.js"
+import { Program, Wallet, AnchorProvider } from "@coral-xyz/anchor"
+import { IDL, Turbin3Prereq } from "./programs/Turbin3_prereq";
+import wallet from "./Turbin3-wallet.json"
+const MPL_CORE_PROGRAM_ID = new PublicKey("CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d");
+
+// We're going to import our keypair from the wallet file
+const from = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+// Create a devnet connection
+const connection = new Connection("https://api.devnet.solana.com");
+
+// Create our anchor provider
+const provider = new AnchorProvider(connection, new Wallet(from), {
+commitment: "confirmed"});
+
+// Create our program
+const program : Program<Turbin3Prereq> = new Program(IDL, provider);
