76a85fcfbf8584c13448de5d8cd3cedce5622b7c
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-06-26 17:52:52 -0700

    2. Claim Token Airdrop

--- prereq-rs/notes.md
+++ prereq-rs/notes.md
@@ -7,3 +7,18 @@ prereq-rs]$ cargo test keygen -- --nocapture
 running 1 test
 
 You've generated a new Solana wallet: FxtSYooJ5Tdpcia7XUvWKMRp7mzChS6kxWVTYSyEehtM
+
+running 1 test
+test tests::airdrop ... ok
+
+successes:
+
+---- tests::airdrop stdout ----
+Success! Check your TX here:
+https://explorer.solana.com/tx/5EBQKCSRHRWZDPY52ABRnqVBBtuuRme9Sg9jip9QR7bjXQxXoFBHRx4t5LQrGrfFztwTdHBkJjYi9ndNKe57FZQ4?cluster=devnet
+
+
+successes:
+    tests::airdrop
+
+test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 4 filtered out; finished in 1.21s
--- prereq-rs/src/lib.rs
+++ prereq-rs/src/lib.rs
@@ -1,58 +1,84 @@
-use solana_sdk::{signature::{Keypair, Signer}, pubkey::Pubkey};
-use bs58; use std::io::{self, BufRead};
+use bs58;
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::{
+    pubkey::Pubkey,
+    signature::{Keypair, Signer, read_keypair_file},
+};
+use std::io::{self, BufRead};
 
+const RPC_URL: &str =
+    "https://turbine-solanad-4cde.devnet.rpcpool.com/9a9da9cf-6db1-47dc-839a-55aca5c9c80a";
 
-#[cfg(test)] mod tests {
+#[cfg(test)]
+mod tests {
     use super::*;
 
-    use solana_sdk;
+    //use solana_sdk;
 
     #[test]
-    fn keygen()
-    {
-        // Create a new keypair 
+    fn keygen() {
+        // Create a new keypair
         let kp = Keypair::new();
-        println!("You've generated a new Solana wallet: {}", kp.pubkey().to_string());
+        println!(
+            "You've generated a new Solana wallet: {}",
+            kp.pubkey().to_string()
+        );
         println!("");
         println!("To save your wallet, copy and paste the following into a JSON file:");
         println!("{:?}", kp.to_bytes());
-        }
-
-    #[test]
-    fn airdrop() {}
-    #[test]
-    fn transfer_sol() {}
     }
 
     #[test]
+    fn airdrop() {
+        // Import our keypair
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        // we'll establish a connection to Solana devnet using the const we defined above
+        let client = RpcClient::new(RPC_URL);
+        // We're going to claim 2 devnet SOL tokens (2 billion lamports)
+        match client.request_airdrop(&keypair.pubkey(), 2_000_000_000u64) {
+            Ok(sig) => {
+                println!("Success! Check your TX here:");
+                println!("https://explorer.solana.com/tx/{}?cluster=devnet", sig);
+            }
+            Err(err) => {
+                println!("Airdrop failed: {}", err);
+            }
+        }
+    }
+
+    #[test]
+    fn transfer_sol() {}
+}
+
+#[test]
 fn base58_to_wallet() {
-println!("Input your private key as a base58 string:");
-let stdin = io::stdin();
-let base58 = stdin.lock().lines().next().unwrap().unwrap();
-println!("Your wallet file format is:");
-let wallet = bs58::decode(base58).into_vec().unwrap();
-println!("{:?}", wallet);
+    println!("Input your private key as a base58 string:");
+    let stdin = io::stdin();
+    let base58 = stdin.lock().lines().next().unwrap().unwrap();
+    println!("Your wallet file format is:");
+    let wallet = bs58::decode(base58).into_vec().unwrap();
+    println!("{:?}", wallet);
 }
 
 #[test]
 fn wallet_to_base58() {
-println!("Input your private key as a JSON byte array (e.g. [12,34,...]):");
-let stdin = io::stdin();
-let wallet = stdin
-.lock()
-.lines()
-.next()
-.unwrap()
-.unwrap()
-.trim_start_matches('[')
-.trim_end_matches(']')
-.split(',')
-.map(|s| s.trim().parse::<u8>().unwrap())
-.collect::<Vec<u8>>();
-println!("Your Base58-encoded private key is:");
+    println!("Input your private key as a JSON byte array (e.g. [12,34,...]):");
+    let stdin = io::stdin();
+    let wallet = stdin
+        .lock()
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim_start_matches('[')
+        .trim_end_matches(']')
+        .split(',')
+        .map(|s| s.trim().parse::<u8>().unwrap())
+        .collect::<Vec<u8>>();
+    println!("Your Base58-encoded private key is:");
 
-let base58 = bs58::encode(wallet).into_string();
-println!("{:?}", base58);
+    let base58 = bs58::encode(wallet).into_string();
+    println!("{:?}", base58);
 }
 
 /* pub fn add(left: u64, right: u64) -> u64 {
