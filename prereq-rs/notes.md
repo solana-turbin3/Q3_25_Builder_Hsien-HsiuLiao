8d947b91adf32063d5710e9105e1c350f0642d3c
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-06-26 17:05:38 -0700

    1. Create a new Keypair

---
+++ prereq-rs/notes.md
@@ -0,0 +1,9 @@
+prereq-rs]$ cargo test keygen -- --nocapture
+
+
+    Finished `test` profile [unoptimized + debuginfo] target(s) in 0.32s
+     Running unittests src/lib.rs (target/debug/deps/prereq_rs-40c36507712e1bf6)
+
+running 1 test
+
+You've generated a new Solana wallet: FxtSYooJ5Tdpcia7XUvWKMRp7mzChS6kxWVTYSyEehtM
---
+++ prereq-rs/src/lib.rs
@@ -0,0 +1,72 @@
+use solana_sdk::{signature::{Keypair, Signer}, pubkey::Pubkey};
+use bs58; use std::io::{self, BufRead};
+
+
+#[cfg(test)] mod tests {
+    use super::*;
+
+    use solana_sdk;
+
+    #[test]
+    fn keygen()
+    {
+        // Create a new keypair 
+        let kp = Keypair::new();
+        println!("You've generated a new Solana wallet: {}", kp.pubkey().to_string());
+        println!("");
+        println!("To save your wallet, copy and paste the following into a JSON file:");
+        println!("{:?}", kp.to_bytes());
+        }
+
+    #[test]
+    fn airdrop() {}
+    #[test]
+    fn transfer_sol() {}
+    }
+
+    #[test]
+fn base58_to_wallet() {
+println!("Input your private key as a base58 string:");
+let stdin = io::stdin();
+let base58 = stdin.lock().lines().next().unwrap().unwrap();
+println!("Your wallet file format is:");
+let wallet = bs58::decode(base58).into_vec().unwrap();
+println!("{:?}", wallet);
+}
+
+#[test]
+fn wallet_to_base58() {
+println!("Input your private key as a JSON byte array (e.g. [12,34,...]):");
+let stdin = io::stdin();
+let wallet = stdin
+.lock()
+.lines()
+.next()
+.unwrap()
+.unwrap()
+.trim_start_matches('[')
+.trim_end_matches(']')
+.split(',')
+.map(|s| s.trim().parse::<u8>().unwrap())
+.collect::<Vec<u8>>();
+println!("Your Base58-encoded private key is:");
+
+let base58 = bs58::encode(wallet).into_string();
+println!("{:?}", base58);
+}
+
+/* pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+ */
