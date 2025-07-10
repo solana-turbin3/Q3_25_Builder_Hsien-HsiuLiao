dd5aefd478b2a2dc93d8a24f9df1ef26b8a16d7b
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-07-09 20:43:06 -0700

    setup tests

--- vault/tests/vault.ts
+++ vault/tests/vault.ts
@@ -6,11 +6,56 @@ describe("vault", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const provider = anchor.getProvider();
+
+  // const connection = provider.connection;
+   const commitment: Commitment = "confirmed";
+ 
+   const connection = new Connection("https://turbine-solanad-4cde.devnet.rpcpool.com/168dd64f-ce5e-4e19-a836-f6482ad6b396", commitment); 
+ 
+
   const program = anchor.workspace.vault as Program<Vault>;
+
+  //helpers
+  const confirm = async (signature: string): Promise<string> => {
+    const block = await connection.getLatestBlockhash();
+    await connection.confirmTransaction({
+      signature,
+      ...block,
+    });
+    return signature;
+  };
+
+  const log = async (signature: string): Promise<string> => {
+    console.log(
+      `Your transaction signature: https://explorer.solana.com/transaction/${signature}?cluster=custom&customUrl=${connection.rpcEndpoint}`
+    );
+    return signature;
+  };
 
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
+  });
+
+  it("should store bumps in vault_state", async () => {
+    
+    //console.log("Your transaction signature", tx);
+  });
+
+  it("should make a deposit", async () => {
+    
+    //console.log("Your transaction signature", tx);
+  });
+
+  it("should withdraw", async () => {
+    
+    //console.log("Your transaction signature", tx);
+  });
+
+  it("should close the vault and vault_state accounts", async () => {
+    
+    //console.log("Your transaction signature", tx);
   });
 });
