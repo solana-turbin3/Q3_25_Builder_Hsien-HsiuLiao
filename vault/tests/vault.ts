b2a97088fdac0c53d1a1abf70d571481ef0576b5
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-07-13 23:09:47 -0700

    update vault test

--- vault/tests/vault.ts
+++ vault/tests/vault.ts
@@ -1,6 +1,17 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Vault } from "../target/types/vault";
+import {
+  Commitment,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  Connection
+} from "@solana/web3.js";
+
+import { assert } from "chai";
 
 describe("vault", () => {
   // Configure the client to use the local cluster.
@@ -15,6 +26,20 @@ describe("vault", () => {
  
 
   const program = anchor.workspace.vault as Program<Vault>;
+
+  let user = Keypair.generate();
+ // let vaultState;
+ // let vault;
+
+  const vaultState = PublicKey.findProgramAddressSync(
+    [Buffer.from("state"), user.publicKey.toBuffer()],
+    program.programId
+  )[0];
+
+  const vault = PublicKey.findProgramAddressSync(
+    [Buffer.from("vault"), vaultState.toBuffer()],
+    program.programId
+  )[0];
 
   //helpers
   const confirm = async (signature: string): Promise<string> => {
@@ -33,10 +58,33 @@ describe("vault", () => {
     return signature;
   };
 
+  before(async () => {
+    // Airdrop some SOL to the user for testing
+    const tx = await connection.requestAirdrop(user.publicKey, 2 * LAMPORTS_PER_SOL);
+    await connection.confirmTransaction(tx);
+    const balance = await connection.getBalance(user.publicKey);
+    console.log(`Balance for user: ${user.publicKey} ${balance / LAMPORTS_PER_SOL} SOL`);
+
+  });
+
+
+
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const balance = await connection.getBalance(user.publicKey);
+    console.log(`Balance for user: ${user.publicKey} ${balance / LAMPORTS_PER_SOL} SOL`);
+    
+   const tx = await program.methods.initialize().accountsStrict({
+      user: user.publicKey,
+      vaultState: vaultState,
+      vault: vault,
+      systemProgram: SystemProgram.programId,
+    }).signers([user]).rpc();
+    
     console.log("Your transaction signature", tx);
+    // Fetch the vault state to check if bumps are stored
+   /*  vaultState = await program.account.vaultState.fetch(vaultState.publicKey);
+    assert.ok(vaultState.vault_bump !== undefined);
+    assert.ok(vaultState.vaultstate_bump !== undefined); */
   });
 
   it("should store bumps in vault_state", async () => {
