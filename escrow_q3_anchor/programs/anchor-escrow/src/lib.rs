bbe8715cb0c12d0f8e8e8c64b88104e66809f4e1
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-07-14 13:38:07 -0700

    add make ix

--- escrow_q3_anchor/programs/anchor-escrow/src/lib.rs
+++ escrow_q3_anchor/programs/anchor-escrow/src/lib.rs
@@ -17,4 +17,15 @@ declare_id!("8KiiqftKSSHTE1zF1XmtcWf1zvppaFf9C7z4mmA46p3H");
 pub mod anchor_escrow {
     use super::*;
 
+    pub fn make(ctx: Context<Make>, seed: u64, receive: u64, deposit: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
+        Ok(())
+    }
+
+   /*  pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close()?;
+        Ok(())
+    } */
 }
