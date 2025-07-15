6c40a40dd9fc3522ae4258154855ce7fa888195e
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-07-15 11:53:14 -0700

    add ix

--- amm/programs/amm/src/lib.rs
+++ amm/programs/amm/src/lib.rs
@@ -1,4 +1,11 @@
+#![allow(unexpected_cfgs, deprecated)]
+
 use anchor_lang::prelude::*;
+
+mod instructions;
+mod state;
+
+use instructions::*;
 
 declare_id!("HdrLqs3W78xGRbfn1oCi4XAkrnTL3yg2DoQt83a95TsM");
 
@@ -6,11 +13,28 @@ declare_id!("HdrLqs3W78xGRbfn1oCi4XAkrnTL3yg2DoQt83a95TsM");
 pub mod amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64, 
+        fee: u16, 
+        authority: Option<Pubkey>,
+
+    ) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)?;
+
         Ok(())
     }
+
+   /*  pub fn deposit(
+        ctx: Context<Deposit>,
+        amount: u64, //amount of lp tokens that depositor wants to get back 
+        max_x: u64, //max x tokens depositor willing to deposit
+        max_y: u64 //max y tokens depositor willing to deposit
+                    //these are needed to avoid slippage
+    ) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)?;
+
+        Ok(())
+    } */
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
