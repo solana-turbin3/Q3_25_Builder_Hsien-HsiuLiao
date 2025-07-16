bfa7cff54534fb402fe18bb29a32884a48ee66d7
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-07-16 14:41:13 -0700

    day 8 class

---
+++ NFT-Staking-q3-2025/programs/nft-staking/src/instructions/claim.rs
@@ -0,0 +1,6 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{associated_token::AssociatedToken, token::{mint_to, Mint, MintTo, Token, TokenAccount}};
+
+use crate::state::{StakeConfig, UserAccount};
+
+//alow user to claim points after a period
