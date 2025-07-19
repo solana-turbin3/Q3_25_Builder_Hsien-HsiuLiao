0c15f917fb794bf0f6a1d80a7304128bfe31b8b4
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-07-19 16:03:41 -0700

    make ix test passing

--- escrow_q3_anchor/tests/anchor-escrow.test.ts
+++ escrow_q3_anchor/tests/anchor-escrow.test.ts
@@ -10,151 +10,144 @@ import {
   Transaction,
   Connection
 } from "@solana/web3.js";
-import { createMint, getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
+import { Account, ASSOCIATED_TOKEN_PROGRAM_ID, createMint, getAssociatedTokenAddressSync, getOrCreateAssociatedTokenAccount, mintTo, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
-    CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
-} from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 
 import { assert } from "chai";
 
-import wallet from "../../Turbin3-wallet.json";
+//import wallet from "../../Turbin3-wallet.json";
 import { min } from "bn.js";
+import { randomBytes } from "crypto";
 
 describe("anchor-escrow", () => {
+
+  // Helper function to confirm a transaction
+  const confirmTx = async (signature: string) => {
+    const latestBlockhash = await anchor.getProvider().connection.getLatestBlockhash();
+    await anchor.getProvider().connection.confirmTransaction(
+      {
+        signature,
+        ...latestBlockhash,
+      },
+      commitment
+    )
+  }
+  const confirmTxs = async (signatures: string[]) => {
+    await Promise.all(signatures.map(confirmTx))
+  }
+
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const provider = anchor.getProvider();
 
- // const connection = provider.connection;
   const commitment: Commitment = "confirmed";
 
-  const connection = new Connection("https://api.devnet.solana.com");
+  const connection = provider.connection;
 
-  const signerkp = Keypair.fromSecretKey(new Uint8Array(wallet));
+  //const signerkp = Keypair.fromSecretKey(new Uint8Array(wallet));
 
-  let makerAtaA;
-  let vault;
-  
+
   const program = anchor.workspace.anchorEscrow as Program<AnchorEscrow>;
 
-  // Create a UMI connection
-const umi = createUmi('https://api.devnet.solana.com');
-const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
-const signer = createSignerFromKeypair(umi, keypair);
-umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
-
-const token_decimals = 1_000_000;
-
-const seed = new anchor.BN(123);
-const receive = new anchor.BN(1000);
-const deposit = new anchor.BN(500);
-
-const escrow = PublicKey.findProgramAddressSync(
-  [Buffer.from("escrow"), signerkp.publicKey.toBuffer(), new Uint8Array(seed.toArray('le', 4))],
-  program.programId
-)[0];
-
-
-
-  before(async () => {
-    const mintA = await createMint(connection, signerkp, signerkp.publicKey, null, 6);
-
-    const umiMint = publicKey(mintA)
-
-    try {
-         let accounts: CreateMetadataAccountV3InstructionAccounts = {
-             mint: umiMint, 
-             mintAuthority: signer
-         }
  
-         let data: DataV2Args = {
-             name: "Can we ship it?",
-             symbol: "CWSI",
-             uri: "http://www.arweave.net/12345",
-             sellerFeeBasisPoints: 3,
-             creators: null,
-             collection: null,
-             uses: null
- 
-         }
- 
-         let args: CreateMetadataAccountV3InstructionArgs = {
-             data,
-             isMutable: true,
-             collectionDetails: null
- 
-         }
- 
-         let tx = createMetadataAccountV3(
-             umi,
-             {
-                 ...accounts,
-                 ...args
-             }
-         )
- 
-         let result = await tx.sendAndConfirm(umi);
-         console.log(bs58.encode(result.signature));
-         /* 
-         https://explorer.solana.com/tx/3jgT2kTzAy43r7fLwUnQjerVwwZxt2FQyo3VN1xuyfFGjEuQe4fzp9MC6Y5pXNhJZEYYXArMWPCDRh7ik8YUmYoK?cluster=devnet
- 
-         the following shows token metadata
-         https://explorer.solana.com/address/6jocG9wDorGF2Y18T7UrhFvJmYN9e36tfkd83VsuXW4V/metadata?cluster=devnet
-         */
-     } catch(e) {
-         console.error(`Oops, something went wrong: ${e}`)
-     }
+
+  const token_decimals = 1_000_000;
+
+  const seed = new anchor.BN(randomBytes(8));
+  const receive = new anchor.BN(1000);
+  const deposit = new anchor.BN(500);
+
+
+
+  const [maker, taker] =
+    [new Keypair(), new Keypair(), new Keypair(), new Keypair(),];
+
+  /* const [makerAtaA, makerAtaB, takerAtaA, takerAtaB] = [maker, taker]
+    .map((a) =>
+      [mintA, mintB].map((m) =>
+        getAssociatedTokenAddressSync(m.publicKey, a.publicKey, false, TOKEN_PROGRAM_ID)
+      )
+    )
+    .flat(); */
+
+  let makerAtaA: Account;
+  let mintA: PublicKey;
+  let mintB: PublicKey;
+
+  let vault: PublicKey;
+
+
+  const escrow = PublicKey.findProgramAddressSync(
+    [Buffer.from("escrow"), maker.publicKey.toBuffer(), seed.toArrayLike(Buffer, "le", 8)],// seed.toBuffer("le", 8)
+    program.programId
+  )[0];
+
+
+
+  it("before", async () => {
+
+    await Promise.all([maker, taker].map(async (k) => {
+
+
+      return await anchor.getProvider().connection.requestAirdrop(k.publicKey, 2 * anchor.web3.LAMPORTS_PER_SOL)
+    })).then(confirmTxs);
+
+
+
+    mintA = await createMint(connection, maker, maker.publicKey, null, 6);
+    console.log("mintA", mintA);
+    mintB = await createMint(connection, taker, taker.publicKey, null, 6);
+
+
+    vault = getAssociatedTokenAddressSync(
+      mintA,
+      escrow,
+      true
+    );
 
     makerAtaA = await getOrCreateAssociatedTokenAccount(
-            connection,
-            signerkp,
-            mintA, 
-            signerkp.publicKey
-        );
+      connection,
+      maker,
+      mintA,
+      maker.publicKey,
 
-         //   Mint to ATA
-         const mintTx = await mintTo(
-          connection,
-          signerkp,
-          mintA,
-          makerAtaA,
-          signerkp,
-          token_decimals
-      )
-        vault = await getOrCreateAssociatedTokenAccount(
-          connection,
-          signerkp,
-          mintA, 
-        escrow
-      );
+    )
 
- });
+
+
+    //   Mint to ATA
+    const mintTx = await mintTo(
+      connection,
+      maker,
+      mintA,
+      makerAtaA.address,
+      maker.publicKey,
+      10000000
+    )
+
+
+  });
 
   it("Is initialized!", async () => {
-    // Add your test here.
-   
 
     const tx = await program.methods
-    .make(seed, receive ,deposit ).accountsPartial({
-      maker: signerkp.publicKey, 
-     // mintA,
-   //   mintB,
-      makerAtaA,
-    //  escrow,
-      vault,
-    //  associatedTokenProgram,
-   //   tokenProgram,
-   //   systemProgram
-    }).rpc();
+      .make(seed, receive, deposit).accountsPartial({
+        maker: maker.publicKey,
+        mintA: mintA,
+        mintB: mintB,
+        makerAtaA: makerAtaA.address,
+        escrow,
+        vault,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: SystemProgram.programId
+      })
+      .signers([maker])
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
