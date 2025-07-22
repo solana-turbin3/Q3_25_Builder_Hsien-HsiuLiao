1a87b8e47b94eeaca64dffe0f4cbbe8abbe137a8
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-07-22 01:10:33 -0700

    add refund ix

--- escrow_q3_anchor/programs/anchor-escrow/src/lib.rs
+++ escrow_q3_anchor/programs/anchor-escrow/src/lib.rs
@@ -23,6 +23,11 @@ pub mod anchor_escrow {
         Ok(())
     }
 
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()?;
+        Ok(())
+    }
+
    /*  pub fn take(ctx: Context<Take>) -> Result<()> {
         ctx.accounts.deposit()?;
         ctx.accounts.withdraw_and_close()?;
