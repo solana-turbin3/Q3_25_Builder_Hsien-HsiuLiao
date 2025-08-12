77c78056b54d772e1f99def22130a0e89055f254
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-08-11 22:13:50 -0700

    add helpers

--- capstone/anchor-loudness/tests/anchor-loudness-test.ts
+++ capstone/anchor-loudness/tests/anchor-loudness-test.ts
@@ -11,6 +11,10 @@ import {
   Transaction,
   Connection
 } from "@solana/web3.js";
+import * as sb from "@switchboard-xyz/on-demand";
+import { assert } from "chai";
+
+import wallet from "../HTurbin3-wallet.json";
 
 describe("anchor-loudness", () => {
   // Configure the client to use the local cluster.
@@ -18,6 +22,27 @@ describe("anchor-loudness", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.anchorLoudness as Program<AnchorLoudness>;
+
+   //helpers
+   const confirm = async (signature: string): Promise<string> => {
+    const block = await connection.getLatestBlockhash();
+    await connection.confirmTransaction({
+      signature,
+      ...block,
+    });
+    return signature;
+  };
+
+  const log = async (signature: string): Promise<string> => {
+    console.log(
+      `Your transaction signature: https://explorer.solana.com/transaction/${signature}?cluster=custom&customUrl=${connection.rpcEndpoint}`
+    );
+    return signature;
+  };
+
+  // get or find accounts
+
+  const admin = Keypair.fromSecretKey(new Uint8Array(wallet)); //("Coop1aAuEqbN3Pm9TzohXvS3kM4zpp3pJZ9D4M2uWXH2");
 
   const config = PublicKey.findProgramAddressSync(
                             [Buffer.from("loudness_config")], 
@@ -34,7 +59,7 @@ describe("anchor-loudness", () => {
   it("Initialize Config Account", async () => {
     const tx = await program.methods.initializeConfig()
     .accountsPartial({
-      admin: provider.wallet.publicKey,
+      admin: admin.publicKey,
       config,
       rewardsMint,
       systemProgram: SystemProgram.programId,
