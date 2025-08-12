533d4dfb01d01993214e3abbc9b40ec186b91d3a
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-08-11 18:22:56 -0700

    add ix

--- capstone/anchor-loudness/programs/anchor-loudness/src/lib.rs
+++ capstone/anchor-loudness/programs/anchor-loudness/src/lib.rs
@@ -2,15 +2,31 @@ use anchor_lang::prelude::*;
 
 declare_id!("3chRm8Uw232StQpy6G5nVAokSz7bTiUN4e6pvB95PRw2");
 
+mod state;
+mod instructions;
+mod errors;
+
+pub use instructions::*;
+pub use state::*;
+pub use errors::*;
+
 #[program]
 pub mod anchor_loudness {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize_config(ctx: Context<InitializeConfig>) -> Result<()> {
+        ctx.accounts.initialize_config( &ctx.bumps)
+    }
+
+    pub fn initialize_user(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.initialize_user(&ctx.bumps)
+    }
+
+   
+
+    pub fn claim(ctx: Context<Claim>) -> Result<()> {
+        ctx.accounts.claim()
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+
