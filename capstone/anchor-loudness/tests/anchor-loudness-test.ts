8845f53b15315b3e099e13c3a4d4888425ddb976
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-08-11 23:17:31 -0700

    update and pass init user and config tests

--- capstone/anchor-loudness/tests/anchor-loudness-test.ts
+++ capstone/anchor-loudness/tests/anchor-loudness-test.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorLoudness } from "../target/types/anchor_loudness";
-import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, getAssociatedTokenAddressSync} from "@solana/spl-token";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, getAssociatedTokenAddressSync } from "@solana/spl-token";
 import {
   Commitment,
   Keypair,
@@ -14,17 +14,20 @@ import {
 import * as sb from "@switchboard-xyz/on-demand";
 import { assert } from "chai";
 
-import wallet from "../HTurbin3-wallet.json";
+import wallet from "../../Turbin3-wallet.json";
 
 describe("anchor-loudness", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const connection = provider.connection;
+
+
   const program = anchor.workspace.anchorLoudness as Program<AnchorLoudness>;
 
-   //helpers
-   const confirm = async (signature: string): Promise<string> => {
+  //helpers
+  const confirm = async (signature: string): Promise<string> => {
     const block = await connection.getLatestBlockhash();
     await connection.confirmTransaction({
       signature,
@@ -33,6 +36,17 @@ describe("anchor-loudness", () => {
     return signature;
   };
 
+  const confirmTx = async (signature: string) => {
+    const latestBlockhash = await anchor.getProvider().connection.getLatestBlockhash();
+    await anchor.getProvider().connection.confirmTransaction(
+      {
+        signature,
+        ...latestBlockhash,
+      },
+      commitment
+    )
+  }
+
   const log = async (signature: string): Promise<string> => {
     console.log(
       `Your transaction signature: https://explorer.solana.com/transaction/${signature}?cluster=custom&customUrl=${connection.rpcEndpoint}`
@@ -40,34 +54,68 @@ describe("anchor-loudness", () => {
     return signature;
   };
 
+  const commitment: Commitment = "confirmed";
+
   // get or find accounts
 
   const admin = Keypair.fromSecretKey(new Uint8Array(wallet)); //("Coop1aAuEqbN3Pm9TzohXvS3kM4zpp3pJZ9D4M2uWXH2");
 
+  const user = Keypair.fromSecretKey(new Uint8Array(wallet)); //("Coop1aAuEqbN3Pm9TzohXvS3kM4zpp3pJZ9D4M2uWXH2");
+
   const config = PublicKey.findProgramAddressSync(
-                            [Buffer.from("loudness_config")], 
-                            program.programId)
-                            [0];
+    [Buffer.from("loudness_config")],
+    program.programId
+  )[0];
 
   const rewardsMint = PublicKey.findProgramAddressSync(
-                                 [Buffer.from("rewards"), 
-                                  config.toBuffer()], 
-                                  program.programId)
-                                  [0];
+    [Buffer.from("rewards"), config.toBuffer()],
+    program.programId
+  )[0];
 
+
+  const userAccount = PublicKey.findProgramAddressSync(
+    [Buffer.from("user"), user.publicKey.toBuffer()],
+    program.programId
+  )[0];
+
+  //airdrop
+  it("Airdrop", async () => {
+    await anchor.getProvider().connection.requestAirdrop(admin.publicKey, 2 * anchor.web3.LAMPORTS_PER_SOL)
+      .then(confirmTx);
+  });
 
   it("Initialize Config Account", async () => {
     const tx = await program.methods.initializeConfig()
-    .accountsPartial({
-      admin: admin.publicKey,
-      config,
-      rewardsMint,
-      systemProgram: SystemProgram.programId,
-      tokenProgram: TOKEN_PROGRAM_ID,
-    })
-    .rpc();
+      .accountsPartial({
+        admin: admin.publicKey,
+        config,
+        rewardsMint,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .signers([admin])
+      .rpc()
+      .then(confirm)
+      .then(log);
+
     console.log("\nConfig Account Initialized!");
     console.log("Your transaction signature", tx);
   });
-  
+
+  it("Initialize User Account", async () => {
+    const tx = await program.methods.initializeUser()
+      .accountsPartial({
+        user: user.publicKey,
+        userAccount,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([user])
+      .rpc()
+      .then(confirm)
+      .then(log);
+
+    console.log("\nUser Account Initialized!");
+    console.log("Your transaction signature", tx);
+  });
+
 });
