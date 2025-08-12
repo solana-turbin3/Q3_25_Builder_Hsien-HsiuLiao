220d8b7e5b8313e6426d5142956700f7864f10cd
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-08-11 20:06:42 -0700

    update config state

--- capstone/anchor-loudness/programs/anchor-loudness/src/state/config.rs
+++ capstone/anchor-loudness/programs/anchor-loudness/src/state/config.rs
@@ -3,6 +3,8 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct Config {
+    pub admin: Pubkey,
+    pub rewards_token_mint: Pubkey,
     pub rewards_bump: u8,
     pub bump: u8,
 }
