823f2f7d3de8ba9146b660242234f8e9520fd2ea
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-08-12 00:12:59 -0700

    add missing modules

--- capstone/anchor-loudness/programs/anchor-loudness/src/instructions/mod.rs
+++ capstone/anchor-loudness/programs/anchor-loudness/src/instructions/mod.rs
@@ -2,8 +2,10 @@ pub mod initialize_config;
 pub mod initialize_user;
 pub mod claim;
 pub mod create_submission;
+pub mod switchboard_feed;
 
 pub use initialize_config::*;
 pub use initialize_user::*;
 pub use claim::*;
 pub use create_submission::*;
+pub use switchboard_feed::*;
--- capstone/anchor-loudness/programs/anchor-loudness/src/state/mod.rs
+++ capstone/anchor-loudness/programs/anchor-loudness/src/state/mod.rs
@@ -1,5 +1,9 @@
 pub mod config;
 pub mod user_account;
+pub mod submission;
+pub mod venue;
 
 pub use config::*;
 pub use user_account::*;
+pub use submission::*;
+pub use venue::*;
