6a2fb7f1a731a030217d8cb8bc6ad0fb4ed87b89
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-08-11 20:07:16 -0700

    pass initialize test

--- capstone/anchor-loudness/tests/anchor-loudness-test.ts
+++ capstone/anchor-loudness/tests/anchor-loudness-test.ts
@@ -1,20 +1,43 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorLoudness } from "../target/types/anchor_loudness";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, getAssociatedTokenAddressSync} from "@solana/spl-token";
+import {
+  Commitment,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  Connection
+} from "@solana/web3.js";
 
 describe("anchor-loudness", () => {
   // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.anchorLoudness as Program<AnchorLoudness>;
 
+  const config = PublicKey.findProgramAddressSync(
+                            [Buffer.from("loudness_config")], 
+                            program.programId)
+                            [0];
+
+  const rewardsMint = PublicKey.findProgramAddressSync(
+                                 [Buffer.from("rewards"), 
+                                  config.toBuffer()], 
+                                  program.programId)
+                                  [0];
+
+
   it("Initialize Config Account", async () => {
-    const tx = await program.methods.initializeConfig(10, 10, 0)
+    const tx = await program.methods.initializeConfig()
     .accountsPartial({
       admin: provider.wallet.publicKey,
       config,
       rewardsMint,
-      systemProgram: anchor.web3.SystemProgram.programId,
+      systemProgram: SystemProgram.programId,
       tokenProgram: TOKEN_PROGRAM_ID,
     })
     .rpc();
