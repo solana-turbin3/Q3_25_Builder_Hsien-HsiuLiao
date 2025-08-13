25817ff011d8f4cdee04640c806abcd1678efe2e
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-08-12 17:19:11 -0700

    update to get correct value from feed

--- capstone/anchor-loudness/programs/anchor-loudness/src/instructions/switchboard_feed.rs
+++ capstone/anchor-loudness/programs/anchor-loudness/src/instructions/switchboard_feed.rs
@@ -1,8 +1,6 @@
-use anchor_lang::{prelude::*, system_program::{Transfer, transfer}};
+use anchor_lang::{prelude::*, solana_program::clock, system_program::{transfer, Transfer}};
 use switchboard_on_demand::on_demand::accounts::pull_feed::PullFeedAccountData;
 //use switchboard_on_demand::prelude::rust_decimal::Decimal;
-
-
 
 
 use crate::{ state::{}};
@@ -12,8 +10,6 @@ pub struct SwitchboardFeed<'info> {
     //safety check: Struct field "feed" is unsafe, but is not documented.   add ///CHECK
         /// CHECK: via switchboard sdk
     pub feed: AccountInfo<'info>,
-
-
     pub system_program: Program<'info, System>,
 
 }
@@ -23,7 +19,6 @@ impl <'info> SwitchboardFeed<'info> {
     pub fn get_feed_data(&mut self) -> Result<()> { 
       // Feed account data
       let feed_account = self.feed.data.borrow();//ctx.accounts.feed.data.borrow();
-
        // Verify that this account is the intended one by comparing public keys
        // if ctx.accounts.feed.key != &specific_pubkey {
        //     throwSomeError
@@ -33,7 +28,18 @@ impl <'info> SwitchboardFeed<'info> {
        // Docs at: https://switchboard-on-demand-rust-docs.web.app/on_demand/accounts/pull_feed/struct.PullFeedAccountData.html
        let feed = PullFeedAccountData::parse(feed_account).unwrap();
       
-       msg!(" data, decibels: {:?}", feed.value().unwrap());
+       let clock = Clock::get()?;
+
+       //this section may not be needed
+       let max_staleness = 200;
+        let min_samples = 7;
+        let only_positive = true;
+
+        let value = feed.get_value(&clock, max_staleness, min_samples, only_positive);
+        msg!("feed value: {:?}", value);
+
+      // msg!(" clock: {:?}", &clock);
+       msg!("decibels: {:?}", feed.value(&clock));
       
        Ok(())
        
