97b6ee9f9965f221e68ed0a6de0cfbe9d8dc0c6c
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-08-14 20:40:08 -0700

    close submission struct

---
+++ capstone/anchor-loudness/programs/anchor-loudness/src/instructions/close_submission.rs
@@ -0,0 +1,45 @@
+use anchor_lang::prelude::*;
+
+use crate::state::{UserAccount, Venue, Submission, Config};
+
+#[derive(Accounts)]
+#[instruction(venue_name: String)]
+pub struct CloseSubmission<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        seeds = [b"loudness_config".as_ref()],
+        bump,
+    )]
+    pub config: Account<'info, Config>,
+    #[account(
+     
+        seeds = [b"user".as_ref(), user.key().as_ref()],
+        bump,
+    )]
+    pub user_account: Account<'info, UserAccount>,
+    #[account(
+       
+        seeds = [config.key().as_ref(), venue_name.as_str().as_bytes()],
+        bump,
+        )]
+    pub venue: Account<'info, Venue>,
+    #[account(
+        mut,     
+        seeds = [venue.key().as_ref(), user.key().as_ref()],
+        bump,
+        close = user,
+ 
+    )]
+    pub submission: Account<'info, Submission>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> CloseSubmission<'info> {
+    
+    pub fn close_submission(&mut self) -> Result<()> {
+        self.user_account.num_of_submissions -= 1;
+
+        Ok(())
+    }
+} 
