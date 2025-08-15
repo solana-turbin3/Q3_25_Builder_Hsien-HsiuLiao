c242b706733d811ff66debd86654ce00315eff10
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-08-14 19:44:54 -0700

    refactor submssion struct

--- capstone/anchor-loudness/programs/anchor-loudness/src/state/submission.rs
+++ capstone/anchor-loudness/programs/anchor-loudness/src/state/submission.rs
@@ -1,12 +1,17 @@
 use anchor_lang::prelude::*;
 
 #[account]
-#[derive(InitSpace)]
+#[derive(InitSpace, PartialEq, Eq)]
 pub struct Submission {
     pub concert_goer: Pubkey,
+    pub sound_level_data: SoundLevelData,
+    pub bump: u8,
+}
+
+#[derive(InitSpace,AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub struct SoundLevelData {
     pub sound_level: u8,
     pub timestamp: i64,
     pub seat_number: u8,
     pub user_rating: u8,
-    pub bump: u8,
 }
